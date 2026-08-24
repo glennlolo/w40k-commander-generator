ff4c0e5d5a07c65782dd98223038c13413a2adc2
glennlolo <hugoblivet1597@hotmail.fr>
2026-08-24 23:06:08 +0200

    Converting unittest to pytest for better logging testing

--- comgen/test/test_ComGen.py
+++ comgen/test/test_ComGen.py
@@ -1,79 +1,77 @@
+import pprint
 import logging
-from unittest import TestCase
-from unittest.mock import patch
+import pytest
 
 from comgen.comgen import ComGen
 from comgen.enums import Race, Rank, Sex, Subtype
 
-class TestGetCommanderParams(TestCase):
+@pytest.fixture
+def options():
+    return {
+        "race": Race.astra.title,
+        "sex": "",
+        "subtype": "",
+        "rank": "",
+        "batch": 1,
+        "export": "json",
+        "mode": "full",
+        "debug": False
+    }
 
-    def setUp(self):
-        self.options = {
-            "race": Race.astra.title,
-            "sex": "",
-            "subtype": "",
-            "rank": "",
-            "batch": 1,
-            "export": "json",
-            "mode": "full",
-            "debug": False
-        }
+def testGetRandomCommanderParams(options):
+    comgen = ComGen(options)
+    params = comgen.getCommandersParams(options)
+    # Check param types and length
+    assert isinstance(params, list)
+    assert len(params) == 1
+    assert isinstance(params[0], dict)
+    assert isinstance(params[0]["race"], Race)
+    assert isinstance(params[0]["sex"], Sex)
+    assert isinstance(params[0]["subtype"], Subtype)
+    assert isinstance(params[0]["rank"], Rank)
+    # Check forced value
+    assert params[0]["race"].title == options["race"]
+    # Check if params are compatible
+    assert params[0]["subtype"].race == params[0]["race"].title
+    assert params[0]["race"].title in params[0]["rank"].race
 
-    def testGetRandomCommanderParams(self):
-        comgen = ComGen(self.options)
-        params = comgen.getCommandersParams(self.options)
-        # Check param types and length
-        self.assertIsInstance(params, list)
-        self.assertEqual(len(params), 1)
-        self.assertIsInstance(params[0], dict)
-        self.assertIsInstance(params[0]["race"], Race)
-        self.assertIsInstance(params[0]["sex"], Sex)
-        self.assertIsInstance(params[0]["subtype"], Subtype)
-        self.assertIsInstance(params[0]["rank"], Rank)
-        # Check forced value
-        self.assertEqual(params[0]["race"].title, self.options["race"])
-        # Check if params are compatible
-        self.assertEqual(params[0]["subtype"].race, params[0]["race"].title)
-        self.assertIn(params[0]["race"].title, params[0]["rank"].race)
+def testGetSpecificCommanderParams(options):
+    options["sex"] = Sex.male.title
+    options["subtype"] = Subtype.mordian.title
+    options["rank"] = Rank.general.title
 
-    def testGetSpecificCommanderParams(self):
-        self.options["sex"] = Sex.male.title
-        self.options["subtype"] = Subtype.mordian.title
-        self.options["rank"] = Rank.general.title
+    comgen = ComGen(options)
+    params = comgen.getCommandersParams(options)
+    # Check values
+    assert params[0]["sex"].title == options["sex"]
+    assert params[0]["subtype"].title == options["subtype"]
+    assert params[0]["rank"].title == options["rank"]
 
-        comgen = ComGen(self.options)
-        params = comgen.getCommandersParams(self.options)
-        # Check values
-        self.assertEqual(params[0]["sex"].title, self.options["sex"])
-        self.assertEqual(params[0]["subtype"].title, self.options["subtype"])
-        self.assertEqual(params[0]["rank"].title, self.options["rank"])
+def testGetIncompatibleCommanderParams(options):
+    options["subtype"] = Subtype.mordian.title
+    options["rank"] = Rank.chaplain.title
 
-    def testGetIncompatibleCommanderParams(self):
-        self.options["subtype"] = Subtype.mordian.title
-        self.options["rank"] = Rank.chaplain.title
+    with pytest.raises(AssertionError):
+        ComGen(options)
 
-        with self.assertRaises(AssertionError):
-            ComGen(self.options)
+def testBatchGeneration(options):
+    options["batch"] = 5
+    comgen = ComGen(options)
+    params = comgen.getCommandersParams(options)
+    # Check batch size and types
+    assert isinstance(params, list)
+    assert len(params) == 5
+    # Check parameter values
+    assert all(isinstance(param, dict) for param in params)
+    assert all(param["race"].title == options["race"] for param in params)
+    assert all(param["subtype"].race == param["race"].title for param in params)
+    assert all(param["race"].title in param["rank"].race for param in params)
 
-    def testBatchGeneration(self):
-        self.options["batch"] = 5
-        comgen = ComGen(self.options)
-        params = comgen.getCommandersParams(self.options)
-        # Check batch size and types
-        self.assertIsInstance(params, list)
-        self.assertEqual(len(params), 5)
-        # Check parameter values
-        self.assertTrue(all(isinstance(param, dict) for param in params))
-        self.assertTrue(all(param["race"].title == self.options["race"] for param in params))
-        self.assertTrue(all(param["subtype"].race == param["race"].title for param in params))
-        self.assertTrue(all(param["race"].title in param["rank"].race for param in params))
-
-    def testDebugLogging(self):
-        self.options["debug"] = True
-        # Check if debug messages are logged
-        logger = logging.getLogger('comgen.comgen')
-        with patch.object(logger, 'info') as mock_info, patch.object(logger, 'debug') as mock_debug:
-            comgen = ComGen(self.options)
-            params = comgen.getCommandersParams(self.options)
-            mock_info.assert_called_with("Generating 1 commanders with the following options: %s", self.options)
-            mock_debug.assert_called_with("Commander #0 generated options: %s", params[0])
+def testDebugLogging(options,caplog):
+    options["debug"] = True
+    comgen = ComGen(options)
+    caplog.clear()
+    with caplog.at_level(logging.DEBUG):
+        params = comgen.getCommandersParams(options)
+        assert caplog.record_tuples == [("comgen.comgen", logging.INFO, "Generating 1 commanders with the following options: \n" + pprint.pformat(options)), 
+                                    ("comgen.comgen", logging.DEBUG, "Commander #0 generated options: \n" + pprint.pformat(params[0]))]
